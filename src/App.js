a626c2cd3b999f6af85f1e24f570db2bc19ce76a
Durgesh Thakur <Durgesh.Thakur@igglobal.com>
2021-11-11 22:56:42 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -11,26 +11,21 @@ import {
 } from "react-router-dom";
 import Detail from "./containers/Listing/Detail";
 import SearchDetail from "./components/SearchDetail";
-
 function App() {
   return (
     <>
       <div className={'container-fluid p-0'}>
-
       <Router>
         <Routes>
-            <Route exact path="/Login" element={<Login />}/>
-            <Route path="/register" element={<Register />}/>
             <Route exact path="/" element={<Listing />}/>
+            <Route exact path="/Login" element={<Login />}/>
+            <Route path="/Register" element={<Register />}/>
             <Route exact path="/Detail" element={<Detail />}/>
-            <Route exact path="/searchdetail" element={<SearchDetail />}/>
-            {/* <Route exact path="/searchdetail/?search=:search" element={<SearchDetail />}/> */}
-            {/* <Route exact path="/searchdetail/:search" element={<SearchDetail />}/> */}
+            <Route exact path="/searchdetail/search:${state}" element={<SearchDetail />}/>
         </Routes>
       </Router>
       </div> 
     </>
   );
 }
-
 export default App;
