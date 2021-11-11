30125be29d170ec214f3b7056af5b28aecfe4eb4
Durgesh Thakur <Durgesh.Thakur@igglobal.com>
2021-11-11 19:43:17 +0530

    updates

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -8,29 +8,39 @@ function Comment() {
             <div className={'comment py-3'}>
                 <img src={'user.png'} className={'circle icon-left'} alt='user' />
                 <div className={'commentContent'}>
-                    <p className={'uname'}>Neil</p>
+                    <p className={'titleText'}>Neil</p>
                     <p className={'commentText'}>Why does it’s eye look so Human?</p>
                 </div>
             </div>
             <div className={'comment py-3'}>
                 <img src={'user.png'} className={'circle icon-left'} alt='user' />
                 <div className={'commentContent'}>
-                    <p className={'uname'}>Steve</p>
+                    <p className={'titleText'}>Steve</p>
                     <p className={'commentText'}>This article is a wonderful change of pace: a giant squid instead of Donald Trump. What a welcome relief.</p>
                 </div>
             </div>
             <div className={'comment py-3'}>
                 <img src={'user.png'} className={'circle icon-left'} alt='user' />
                 <div className={'commentContent'}>
-                    <p className={'uname'}>Moodbeast</p>
+                    <p className={'titleText'}>Moodbeast</p>
                     <p className={'commentText'}>Why couldn't this a be 20 second video? Man I love the ocean.</p>
                 </div>
             </div>
             <div className={'comment py-3'}>
                 <img src={'user.png'} className={'circle icon-left'} alt='user' />
                 <div className={'commentContent'}>
-                    <p className={'uname'}>Neil</p>
+                    <p className={'titleText'}>Neil</p>
                     <p className={'commentText'}>So genuinely excited it’s Cephalopod week in the NYTimes - my husband’s currently reading « Other Minds » a very readable book examining how Octopus intelligence evolved separately to human intelligence. Can’t wait to see what tomorrow brings.</p>
+                </div>
+                <div className={'replyCommentWrap'}>
+                    <div className={'comment py-3'}>
+                        <p className={'titleText'}>Reply</p>
+                        <img src={'user.png'} className={'circle icon-left'} alt='user' />
+                        <div className={'commentContent'}>
+                            <p className={'titleText'}>Helen</p>
+                            <p className={'commentText'}>@Helen\nOther Minds - great book!</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
